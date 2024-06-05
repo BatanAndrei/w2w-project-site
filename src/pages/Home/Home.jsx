45e4d67203482cb57efeb629603a3b16396d0cd6
BatanAndrei <yabatan777@gmail.com>
2024-06-05 21:18:59 +0300

    initial commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,29 @@
+import styles from './home.module.scss';
+
+
 const Home = () => {
-    
+
     return (
-        <div>
-            <h1>Главная</h1>
-        </div>
+        <>
+            <div className={styles.headWrapper}>
+                
+            </div>
+            <div className={styles.trackerWrapper}>
+                
+            </div>
+            <div className={styles.sliderWrapper}>
+                
+            </div>
+            <div className={styles.sliderPartnersWrapper}>
+                
+            </div>
+            <div className={styles.collabsWrapper}>
+                
+            </div>
+            <div className={styles.footerWrapper}>
+                
+            </div>
+        </>
     )
 };
 
