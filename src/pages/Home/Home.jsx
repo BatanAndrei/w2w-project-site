8a320ae4b85916ca60108fd55d5414b47d5bc587
BatanAndrei <yabatan777@gmail.com>
2024-06-05 22:06:26 +0300

    initial commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import styles from './home.module.scss';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
@@ -6,7 +7,7 @@ const Home = () => {
     return (
         <>
             <div className={styles.headWrapper}>
-                
+                <Link to='/signUp-create/confirm-phone'>НАЗАД к проверочному номеру</Link>
             </div>
             <div className={styles.trackerWrapper}>
                 
@@ -17,9 +18,11 @@ const Home = () => {
             <div className={styles.sliderPartnersWrapper}>
                 
             </div>
+            <div className={styles.wrapperTitlePartners}><h2 className={styles.titlePartners}>Лучшие партнёры</h2></div>
             <div className={styles.collabsWrapper}>
                 
             </div>
+            <div className={styles.wrapperTitleCollaba}><h2 className={styles.titleCollaba}>Коллаба недели</h2></div>
             <div className={styles.footerWrapper}>
                 
             </div>
