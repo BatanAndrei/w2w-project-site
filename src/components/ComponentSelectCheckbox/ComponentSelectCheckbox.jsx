983f6a2cab72ab366917f7c40f444321387e97e8
BatanAndrei <yabatan777@gmail.com>
2024-06-17 19:54:54 +0300

    initial commit

--- src/components/ComponentSelectCheckbox/ComponentSelectCheckbox.jsx
+++ src/components/ComponentSelectCheckbox/ComponentSelectCheckbox.jsx
@@ -6,12 +6,13 @@ const ComponentSelectCheckbox = ({dataListItems, classNameInputTextSelect, class
 
     const [displayCheckboxes, setDisplayCheckboxes] = useState(false);
     const [fillDataFromCheckboxes, setFillDataFromCheckboxes] = useState([]);
+    const [allListItems, setAllListItems] = useState(dataListItems)
 
     const handleDropeList = () => {
         setDisplayCheckboxes(() => !displayCheckboxes);
     };
 
-    const handleChangeCheckbox = (e) => {
+    const handleChangeCheckbox = (e, item) => {
         if(e.target.checked) {
             setFillDataFromCheckboxes(pre => [...pre, e.target.name])
         }else {
@@ -19,15 +20,19 @@ const ComponentSelectCheckbox = ({dataListItems, classNameInputTextSelect, class
                 return [...pre.filter(item => item !== e.target.name)]
             })
         };
+        
+        const id = item.id
+        let checkedItems = allListItems.map((item) => item.id === id ? {...item, checked: !item.checked, count: 1} : item);
+        setAllListItems(checkedItems);
     };
-    console.log(fillDataFromCheckboxes.join(', '))
+    
     return (
         <>
             <input onClick={handleDropeList} value={fillDataFromCheckboxes.join(', ')} className={classNameInputTextSelect} type='type' {...register} {...rest} readonly/>
             {displayCheckboxes && <div className={styles.containerDropeList}>
                 <div className={styles.wrapperCheckboxes}>
-                    {dataListItems.map((item, index) => <label className={classNamePositionLableSelect} key={index}>
-                    <input onChange={(e) => handleChangeCheckbox(e)} type={type} name={item.title} id={item.id} className={styles.checkbox}/>
+                    {allListItems.map((item, index) => <label className={classNamePositionLableSelect} key={index}>
+                    <input onChange={(e) => handleChangeCheckbox(e, item)} checked={item.checked} type={type} name={item.title} id={item.id} className={styles.checkbox}/>
                     <span className={styles.fake}></span>
                     <div className={classNameTextTitleSelect}>{item.title}</div>
                 </label>)}
