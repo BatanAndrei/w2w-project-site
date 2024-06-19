2ea625645f2a222e3ceb98c5a65bfc2db996af81
BatanAndrei <yabatan777@gmail.com>
2024-06-19 22:20:28 +0300

    initial commit

--- src/components/ComponentCheckBadge/ComponentCheckBadge.jsx
+++ src/components/ComponentCheckBadge/ComponentCheckBadge.jsx
@@ -2,11 +2,19 @@ import styles from './componentCheckBladge.module.scss';
 import { useState } from 'react';
 
 
-const ComponentCheckBadge = ({textAreaOnOff, placeholder ,dataListItems, classNameTextTitleSelect, classNamePositionLableSelect, type, register, ...rest}) => {
+const ComponentCheckBadge = ({dataListItems, classNameCheckBadge, classNameChangeColorBadge}) => {
+
+    const [allListItems, setAllListItems] = useState(dataListItems);
+
+    const handleChangeBadge = (item) => {
+        const id = item.id
+        let checkedItems = allListItems.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
+        setAllListItems(checkedItems);
+    }
 
     return (
-        <>  
-            
+        <>
+            {allListItems.map((item) => <div onClick={() => handleChangeBadge(item)} className={item.checked ? classNameChangeColorBadge : classNameCheckBadge}>{item.title}</div>)}
         </>
     )
 };
--- src/pages/TheBrandQuestionnireThird/TheBrandQuestionnireThird.jsx
+++ src/pages/TheBrandQuestionnireThird/TheBrandQuestionnireThird.jsx
@@ -5,6 +5,8 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useNavigate } from "react-router-dom";
 import ComponentCheckBadge from '../../components/ComponentCheckBadge/ComponentCheckBadge';
+import { choiseValuesAnswerData, nameButtonNext,nameButtonFillLeter } from '../../datas/datas';
+import Button from '../../components/Button/Button';
 
 
 const defaultValues = {
@@ -35,23 +37,27 @@ const TheBrandQuestionnireThird = () => {
                 <p className={styles.textSubtitle}>Чтобы заинтересовать потенциального партнера опишите суть и ключевую миссию вашего бизнеса</p>
             </div>
             <form className={styles.form} onSubmit={handleSubmit(dataQuestionnireThird)}>
-            <FieldTextArea
-                register={{...register("missionBusiness")}}
-                autoComplete="off"
-                placeholder="Например, миссия бренда LVL — праздник каждый день"
-                className={styles.inputTextArea}
-                type={'text'}
-            />
-            <div className={styles.countWords}><p className={styles.textCount}>0/50</p></div>
-            <div className={styles.wrapperSubtitle}>
-                <p className={styles.textSubtitle}>Выберите 3 ценности, наиболее подходящие вашему бренду (по ним будут выставляться совпадения на мэтч)</p>
-            </div>
-            <div className={styles.wrapperChoiseValues}>
-                <ComponentCheckBadge
-                
+                <FieldTextArea
+                    register={{...register("missionBusiness")}}
+                    autoComplete="off"
+                    placeholder="Например, миссия бренда LVL — праздник каждый день"
+                    className={styles.inputTextArea}
+                    type={'text'}
                 />
-            </div>
+                <div className={styles.countWords}><p className={styles.textCount}>0/50</p></div>
+                <div className={styles.wrapperSubtitle}>
+                    <p className={styles.textSubtitle}>Выберите 3 ценности, наиболее подходящие вашему бренду (по ним будут выставляться совпадения на мэтч)</p>
+                </div>
+                <div className={styles.wrapperChoiseValues}>
+                    <ComponentCheckBadge
+                        dataListItems={choiseValuesAnswerData}
+                        classNameCheckBadge={styles.checkBadge}
+                        classNameChangeColorBadge={styles.changeBadge}
+                    />
+                </div>
+                <Button className={styles.button} name={nameButtonNext} type="submit"/>
             </form>
+            <Button click={()=> navigate('/')} className={styles.buttonFillLeter} name={nameButtonFillLeter}/>
         </div>
     )
 };
