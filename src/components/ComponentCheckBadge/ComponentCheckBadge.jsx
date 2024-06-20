9b906b318b168637125c646c83afccf7dc327ee0
BatanAndrei <yabatan777@gmail.com>
2024-06-20 12:32:07 +0300

    initial commit

--- src/components/ComponentCheckBadge/ComponentCheckBadge.jsx
+++ src/components/ComponentCheckBadge/ComponentCheckBadge.jsx
@@ -5,16 +5,29 @@ import { useState } from 'react';
 const ComponentCheckBadge = ({dataListItems, classNameCheckBadge, classNameChangeColorBadge}) => {
 
     const [allListItems, setAllListItems] = useState(dataListItems);
+    const [fillDataFromBadge, setFillDataFromBadge] = useState([]);
 
-    const handleChangeBadge = (item) => {
+    const handleChangeBadge = (e, item) => {
+        if(e.target.checked) {
+            setFillDataFromBadge(pre => [...pre, e.target.name]);
+        }else {
+            setFillDataFromBadge(pre => {
+                return [...pre.filter(item => item !== e.target.name)];
+            })
+        };
+
         const id = item.id
         let checkedItems = allListItems.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
         setAllListItems(checkedItems);
     }
-
+console.log(fillDataFromBadge)
     return (
         <>
-            {allListItems.map((item) => <div onClick={() => handleChangeBadge(item)} className={item.checked ? classNameChangeColorBadge : classNameCheckBadge}>{item.title}</div>)}
+            {allListItems.map((item, index) => <label key={index}>
+                    <input onChange={(e) => handleChangeBadge(e, item)} checked={item.checked} type='checkbox' name={item.title} id={item.id} className={styles.checkbox}/>
+                    <span></span>
+                    <div className={item.checked ? classNameChangeColorBadge : classNameCheckBadge}>{item.title}</div>
+                </label>)}
         </>
     )
 };
