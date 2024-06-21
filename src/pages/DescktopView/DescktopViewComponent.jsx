fff9ed7a6a6f0b110139fcbb825af180611f84ea
BatanAndrei <yabatan777@gmail.com>
2024-06-21 16:13:11 +0300

    initial commit

--- src/pages/DescktopView/DescktopViewComponent.jsx
+++ src/pages/DescktopView/DescktopViewComponent.jsx
@@ -10,7 +10,7 @@ const DescktopViewComponent = () => {
                 <LogoDescktop/>
             </div>
             <div className={styles.description}>
-                <h1 className={styles.title}>Скоро сервис<br/> коллабораций будет<br/> на всех экранах</h1>
+                <h1 className={styles.title}>СКОРО СЕРВИС<br/> КОЛЛАБОРАЦИЙ БУДЕТ<br/> НА ВСЕХ ЭКРАНАХ</h1>
                 <h2 className={styles.subTitle}>А пока зайдите к нам с <span className={styles.span}>мобильного телефона</span></h2>
             </div>
             <div className={styles.woolfAnimation}></div>
